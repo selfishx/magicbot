795abf09dd24645b3a586fc54886f2936da9b816
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-03-18 19:26:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,8 +22,21 @@ bot = TelegramClient(None, api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e"
 
 )
 
+
 logging.info("Starting bot...")
 
+
+@bot.on(
+
+    events.NewMessage(
+
+        incoming=True, func=lambda e: not e.is_private))
+
+async def mrunal(event):
+
+    await event.delete()
+
+    return
 
                 
 
