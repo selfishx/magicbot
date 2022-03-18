23240ffdaa5afbf8083294a238b9ece2211da6b3
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-03-18 19:24:01 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+# < (c) 2021 @Godmrunal >
+
+import logging
+
+from os import remove
+
+import requests
+
+from decouple import config
+
+from telethon import Button, TelegramClient, events
+
+logging.basicConfig(
+
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+
+)
+
+bot = TelegramClient(None, api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e").start(
+
+    bot_token=config("BOT_TOKEN")
+
+)
+
+logging.info("Starting bot...")
+
+
+                
+
+logging.info("\n\nBot has started.\n(c) @Godmrunal")
+
+bot.run_until_disconnected()
